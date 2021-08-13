02095a7d27c5ec1799494e11ef1e4e1f1c024e18
Regina2308 <rasmitaraine23.19@ichat.sp.edu.sg>
2021-08-13 18:14:51 +0800

    updated accout settings pt3

--- public/javascript/settings.js
+++ public/javascript/settings.js
@@ -103,11 +103,6 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-
 //profile
 
 
@@ -127,3 +122,18 @@ function readURL(){
 }
 
 
+//edit and update profile
+
+jQuery(function ($) {
+  var $inputs = $('#contact_form :input').prop('disabled', true);
+  $('#edit_btn').click(function () {
+    $("#submit_btn").show(0);
+    $("#edit_btn").hide(0);
+      $inputs.prop('disabled', false);
+  });
+  $('#submit_btn').click(function () {
+      $inputs.prop('disabled', true);
+       $("#submit_btn").hide(0);
+    $("#edit_btn").show(0);
+  });
+})
